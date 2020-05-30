aa109fabd36d8acf296bc15eec82825ad09969ce
Vitalii Tim <post.vitaliy@gmail.com>
2020-05-30 15:02:21 +0300

    Initial Commit

---
+++ WashHands/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WashHands
+//
+//  Created by Vitaliy Tim on 30.05.2020.
+//  Copyright © 2020 Vitalii Tymoshenko. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
